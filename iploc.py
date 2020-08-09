099e8ab095857f257fba8d03cb023658ec0c4342
jeebe <jeebemathew@gmail.com>
2020-08-09 18:48:33 +0000

    result modified

--- iploc.py
+++ iploc.py
@@ -4,4 +4,6 @@ ip = input ('enter IP address : ')
 url = 'http://api.ipstack.com/{}?access_key={}'.format(ip,access_key)
 response = requests.get(url)
 geodata = response.json()
-print(geodata['country_name'])
+country_name = geodata['country_name']
+country_code = geodata['country_code']
+print( ' Country code of the provided IP address:{} is {}  which represents the country {}'.format(ip,country_code,country_name))
